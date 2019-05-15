40fea3ce834cdd3a9c466022de06435a46ac6ebe
PaBLovko <45830475+PaBLovko@users.noreply.github.com>
2019-05-15 21:25:56 +0300

    Update CalcMinNumberTest.java

--- src/test/java/CalcMinNumberTest.java
+++ src/test/java/CalcMinNumberTest.java
@@ -3,20 +3,27 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static org.junit.Assert.*;
 
 public class CalcMinNumberTest {
+    private static Logger log = Logger.getLogger(CalcMinNumberTest.class.getName());
 
     @Test
-    public void minNumberNotResursive() throws MyException{
-        assertEquals("Минимальное число", 3, new CalcMinNumber(Arrays.asList(50, 19, 3, 4, 42))
-                .minNumberNotResursive());
+    public void minNumberNotResursive(){
+        try {
+            assertEquals("Минимальное число", 3, new CalcMinNumber(Arrays.asList(50, 19, 3, 4, 42))
+                    .minNumberNotResursive());
+        }catch (MyException e) {log.log(Level.SEVERE, "Exception: ", e);}
     }
 
     @Test
-    public void minNumberResursive() throws MyException{
-        assertEquals("Минимальное число",3, new CalcMinNumber(Arrays.asList(50, 19, 3, 4, 42))
-                .minNumberResursive());
+    public void minNumberResursive(){
+        try {
+            assertEquals("Минимальное число",3, new CalcMinNumber(Arrays.asList(50, 19, 3, 4, 42))
+                    .minNumberResursive());
+        }catch (MyException e) {log.log(Level.SEVERE, "Exception: ", e);}
     }
 }
