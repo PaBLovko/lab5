98e525c58904f90c837df5445f9d230c41381570
PaBLovko <45830475+PaBLovko@users.noreply.github.com>
2019-05-15 21:26:12 +0300

    Update FindPrimeNumbersTest.java

--- src/test/java/FindPrimeNumbersTest.java
+++ src/test/java/FindPrimeNumbersTest.java
@@ -4,15 +4,20 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static org.junit.Assert.*;
 
 public class FindPrimeNumbersTest {
+    private static Logger log = Logger.getLogger(FindPrimeNumbersTest.class.getName());
 
     @Test
-    public void primeNumbersRecursive() throws MyException{
-        assertThat("Массив простых чисел",new FindPrimeNumbers(Arrays.asList(50, 11, 3, 4, 42))
-                .primeNumbersRecursive(),CoreMatchers.hasItems(3,11));
+    public void primeNumbersRecursive(){
+        try {
+            assertThat("Массив простых чисел",new FindPrimeNumbers(Arrays.asList(50, 11, 3, 4, 42))
+                    .primeNumbersRecursive(),CoreMatchers.hasItems(3,11));
+        }catch (MyException e) {log.log(Level.SEVERE, "Exception: ", e);}
     }
 
     @Test
@@ -21,8 +26,10 @@ public class FindPrimeNumbersTest {
     }
 
     @Test
-    public void primeNumberNotRecursive() throws MyException{
-        assertThat("Массив простых чисел", new FindPrimeNumbers(Arrays.asList(50, 11, 3, 4, 42))
-                .primeNumberNotRecursive(),CoreMatchers.hasItems(3,11));
+    public void primeNumberNotRecursive(){
+        try {
+            assertThat("Массив простых чисел", new FindPrimeNumbers(Arrays.asList(50, 11, 3, 4, 42))
+                    .primeNumberNotRecursive(),CoreMatchers.hasItems(3,11));
+        }catch (MyException e) {log.log(Level.SEVERE, "Exception: ", e);}
     }
 }
