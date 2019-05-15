0a3dc1de5dd439cb1d46bbc5f9bc2001b717e226
PaBLovko <45830475+PaBLovko@users.noreply.github.com>
2019-05-15 08:33:13 +0300

    Update FindPrimeNumbersTest.java

--- src/test/java/FindPrimeNumbersTest.java
+++ src/test/java/FindPrimeNumbersTest.java
@@ -10,12 +10,9 @@ import static org.junit.Assert.*;
 public class FindPrimeNumbersTest {
 
     @Test
-    public void primeNumbersRecursive(){
-        try{
-            assertThat("Массив простых чисел",new FindPrimeNumbers(Arrays.asList(50, 11, 3, 4, 42))
-                    .primeNumbersRecursive(),CoreMatchers.hasItems(3,11));
-        }catch (MyException e) {System.out.println("Error. Not found elem");}
-
+    public void primeNumbersRecursive() throws MyException{
+        assertThat("Массив простых чисел",new FindPrimeNumbers(Arrays.asList(50, 11, 3, 4, 42))
+                .primeNumbersRecursive(),CoreMatchers.hasItems(3,11));
     }
 
     @Test
@@ -24,11 +21,8 @@ public class FindPrimeNumbersTest {
     }
 
     @Test
-    public void primeNumberNotRecursive(){
-        try{
-            assertThat("Массив простых чисел", new FindPrimeNumbers(Arrays.asList(50, 11, 3, 4, 42))
-                    .primeNumberNotRecursive(),CoreMatchers.hasItems(3,11));
-        }catch (MyException e) {System.out.println("Error. Not found elem");}
-
+    public void primeNumberNotRecursive() throws MyException{
+        assertThat("Массив простых чисел", new FindPrimeNumbers(Arrays.asList(50, 11, 3, 4, 42))
+                .primeNumberNotRecursive(),CoreMatchers.hasItems(3,11));
     }
 }
