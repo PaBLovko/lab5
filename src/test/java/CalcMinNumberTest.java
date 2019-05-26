b22a232adc20ee84c8af464a95b20165c103462f
PaBLovko <45830475+PaBLovko@users.noreply.github.com>
2019-05-26 21:26:42 +0300

    Update CalcMinNumberTest.java

--- src/test/java/CalcMinNumberTest.java
+++ src/test/java/CalcMinNumberTest.java
@@ -12,18 +12,14 @@ public class CalcMinNumberTest {
     private static Logger log = Logger.getLogger(CalcMinNumberTest.class.getName());
 
     @Test
-    public void minNumberNotResursive(){
-        try {
-            assertEquals("Минимальное число", 3, new CalcMinNumber(Arrays.asList(50, 19, 3, 4, 42))
-                    .minNumberNotResursive());
-        }catch (MyException e) {log.log(Level.SEVERE, "Exception: ", e);}
+    public void minNumberNotResursive() throws MyException{
+        assertEquals("Минимальное число", 3, new CalcMinNumber(Arrays.asList(50, 19, 3, 4, 42))
+                .minNumberNotResursive());
     }
 
     @Test
-    public void minNumberResursive(){
-        try {
-            assertEquals("Минимальное число",3, new CalcMinNumber(Arrays.asList(50, 19, 3, 4, 42))
-                    .minNumberResursive());
-        }catch (MyException e) {log.log(Level.SEVERE, "Exception: ", e);}
+    public void minNumberResursive() throws MyException{
+        assertEquals("Минимальное число",3, new CalcMinNumber(Arrays.asList(50, 19, 3, 4, 42))
+                .minNumberResursive());
     }
 }
