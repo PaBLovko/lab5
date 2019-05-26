38125758e72b08c06a684c3c5e6233bd495c655f
PaBLovko <45830475+PaBLovko@users.noreply.github.com>
2019-05-26 21:27:00 +0300

    Update FindPrimeNumbersTest.java

--- src/test/java/FindPrimeNumbersTest.java
+++ src/test/java/FindPrimeNumbersTest.java
@@ -1,23 +1,21 @@
 import org.hamcrest.CoreMatchers;
-import org.junit.Test;
+        import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+        import java.util.ArrayList;
+        import java.util.Arrays;
+        import java.util.List;
+        import java.util.logging.Level;
+        import java.util.logging.Logger;
 
-import static org.junit.Assert.*;
+        import static org.junit.Assert.*;
 
 public class FindPrimeNumbersTest {
     private static Logger log = Logger.getLogger(FindPrimeNumbersTest.class.getName());
 
     @Test
-    public void primeNumbersRecursive(){
-        try {
-            assertThat("Массив простых чисел",new FindPrimeNumbers(Arrays.asList(50, 11, 3, 4, 42))
-                    .primeNumbersRecursive(),CoreMatchers.hasItems(3,11));
-        }catch (MyException e) {log.log(Level.SEVERE, "Exception: ", e);}
+    public void primeNumbersRecursive() throws MyException{
+        assertThat("Массив простых чисел",new FindPrimeNumbers(Arrays.asList(50, 11, 3, 4, 42))
+                .primeNumbersRecursive(),CoreMatchers.hasItems(3,11));
     }
 
     @Test
@@ -26,10 +24,8 @@ public class FindPrimeNumbersTest {
     }
 
     @Test
-    public void primeNumberNotRecursive(){
-        try {
-            assertThat("Массив простых чисел", new FindPrimeNumbers(Arrays.asList(50, 11, 3, 4, 42))
-                    .primeNumberNotRecursive(),CoreMatchers.hasItems(3,11));
-        }catch (MyException e) {log.log(Level.SEVERE, "Exception: ", e);}
+    public void primeNumberNotRecursive() throws MyException{
+        assertThat("Массив простых чисел", new FindPrimeNumbers(Arrays.asList(50, 11, 3, 4, 42))
+                .primeNumberNotRecursive(),CoreMatchers.hasItems(3,11));
     }
 }
